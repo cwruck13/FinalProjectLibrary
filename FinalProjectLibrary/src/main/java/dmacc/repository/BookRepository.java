c77c46c0626d4c8e7bac2270218115e361bf1659
harsha malhotra <hmalhotra@dmacc.edu>
2020-11-13 11:37:44 -0600

    add functionality to add a new book

--- FinalProjectLibrary/src/main/java/dmacc/repository/BookRepository.java
+++ FinalProjectLibrary/src/main/java/dmacc/repository/BookRepository.java
@@ -1,5 +1,9 @@
 package dmacc.repository;
 
-public interface BookRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import dmacc.beans.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
