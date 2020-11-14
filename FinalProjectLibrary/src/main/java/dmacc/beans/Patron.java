bb80487235fa950a7d64bc8a8a50a45dc30e4793
cwruck13 <66025028+cwruck13@users.noreply.github.com>
2020-11-14 13:56:07 -0600

    Created brief Patron

---
+++ FinalProjectLibrary/src/main/java/dmacc/beans/Patron.java
@@ -0,0 +1,20 @@
+package dmacc.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Patron {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String name;
+}
