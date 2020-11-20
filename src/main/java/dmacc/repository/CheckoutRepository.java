fec1ed4afe3722995dda1668c68ffc7a70e3cd2b
harsha malhotra <hmalhotra@dmacc.edu>
2020-11-19 21:35:52 -0600

    fix add logic for Patron

--- src/main/java/dmacc/repository/CheckoutRepository.java
+++
@@ -1,9 +0,0 @@
-package dmacc.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dmacc.beans.Checkout;
-
-public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
-
-}
